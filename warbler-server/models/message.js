621643c6d815e63b63eba7580ab30d1ab26254db
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-03 15:43:02 +0200

    message routes and authentication

--- warbler-server/models/message.js
+++ warbler-server/models/message.js
@@ -11,16 +11,20 @@ const messageSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
-});
+}, {
+    timestamps: true
+    }
+);
 
-messageSchema.pre('remove', async function(next) {
+messageSchema.pre('remove', async function (next) {
     try {
         let user = await User.findById(this.user);
         user.messages.remove(this.id);
         await user.save();
         return next();
-         
-    } catch (err) {
+
+    }
+    catch (err) {
         return next(err);
     }
 });
